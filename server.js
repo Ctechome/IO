cc379d287a666ec19001a7bb83a2adac30c16359
Ctechome <miguel.lopez.beca@gmail.com>
2026-05-11 15:04:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "Index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
